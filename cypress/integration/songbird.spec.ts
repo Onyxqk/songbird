dafd605fc5a0af56f680520b64b4041e89ce999c
Tyler Dixon <onyxqk@gmail.com>
2022-02-06 15:20:19 -0500

    full cypress test for initial page load

--- cypress/integration/songbird.spec.ts
+++ cypress/integration/songbird.spec.ts
@@ -1,7 +1,7 @@
 describe('Scenario: Initial load', () => {
 
     describe('Given the user launches Songbird' + ' When the page is loaded', () => {
-        
+
         // load Songbird running on localhost
         before(() => {
             cy.visit('localhost:4200/')
@@ -15,14 +15,22 @@ describe('Scenario: Initial load', () => {
             cy.get('#toolbar').should('be.visible')
         })
 
+        it('And the toolbar text is Songbird', () => {
+            cy.get('#toolbar').contains('Songbird')
+        })
+
         it('And the search component is displayed', () => {
             cy.get('#search').should('be.visible')
+        })
+
+        it('And the search placeholder text is Search for a song or music artist', () => {
+            cy.get('#search').invoke('attr', 'placeholder').should('eq', 'Search for a song or music artist')
         })
 
         it('And there are no detected accessibility violations', () => {
             // inject Axe accessibility testing
             cy.injectAxe()
-            // test the page at initial load
+            // test the page for accessibility
             cy.checkA11y()
         })
     })
