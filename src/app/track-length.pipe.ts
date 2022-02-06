72ef4d181149f5dd9ed6e3f0200b852ddbee871a
Tyler Dixon <onyxqk@gmail.com>
2022-02-05 21:13:57 -0500

    fix track length formatting

--- src/app/track-length.pipe.ts
+++ src/app/track-length.pipe.ts
@@ -8,7 +8,7 @@ export class TrackLengthPipe implements PipeTransform {
   transform(value: number): string {
     let minutes = value/60000
     let seconds = minutes % 1 * 60
-    return Math.trunc(minutes) + ":" + Math.trunc(seconds);
+    return Math.trunc(minutes) + ":" + Math.trunc(seconds).toString().padStart(2, '0');
   }
 
 }
